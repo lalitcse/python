fd2049725dc4c970810e10b0a2c173ba9d086ee3
Lalit Kumar <lalitcse1@gmail.com>
2018-09-23 20:06:53 +0530

    Average of 3 numbers (python 3.6)

---
+++ #001/first.py
@@ -0,0 +1,5 @@
+num1 = input('Enter 1st number : ')
+num2 = input('Enter 2nd number : ')
+num3 = input('Enter 3rd number : ')
+
+print(f"Average of three number : {(int(num1) + (int(num2) + (int(num3)) / 3}")
